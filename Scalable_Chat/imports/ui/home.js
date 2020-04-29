e28df68a2df10bc33ef865d5fae3d557f3227d98
Marx.W <boyuan@vt.edu>
2020-04-29 12:28:57 -0400

    add moment.js timestamp now correct

--- Scalable_Chat/imports/ui/home.js
+++ Scalable_Chat/imports/ui/home.js
@@ -4,49 +4,38 @@ import {
 import {
     Template
 } from 'meteor/templating';
-import './home.html'; 
+import './home.html';
 import {
     Channels
-} from '../api/channels.js'; 
+} from '../api/channels.js';
 // subscription to the 'channels' publication in the home template
-Template.home.onCreated
-(
-    function()
-    {
+Template.home.onCreated(
+    function () {
         this.subscribe('channels');
         this.subscribe('allUserNames');
     }
 );
 
 //helper to show (find) all channels in home 
-Template.home.helpers
-(
-    {
-        channels : function()
-        {
-            return Channels.find();
-        }
+Template.home.helpers({
+    channels: function () {
+        return Channels.find();
     }
-);
+});
 
 // event handler to insert a channel
-Template.channelForm.events
-(
-    {
-        'submit form' : function(event, instance)
-        {
-            event.preventDefault();
-            var name = instance.find('input').value;
-            instance.find('input').value = '';
-            if (name != '')
-                Channels.insert({name : name});
-        }
+Template.channelForm.events({
+    'submit form': function (event, instance) {
+        event.preventDefault();
+        var name = instance.find('input').value;
+        instance.find('input').value = '';
+        if (name != '')
+            Channels.insert({
+                name: name
+            });
     }
-);
+});
 
-Accounts.ui.config
-(
-    {
-        passwordSignupFields : 'USERNAME_AND_EMAIL'
-    }
-);
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
